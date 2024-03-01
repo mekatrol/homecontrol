1c72e81cda3baf080d7fd43524420340182c94b2
PW <mekatrol@wojcik.com.au>
2024-03-01 18:39:15 +1100

    Initial commit

---
+++ homecontrol-dashboard/src/components/SidebarSlider.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="sidebar-slider">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps<{
+  msg: string;
+}>();
+</script>
---
+++ homecontrol-dashboard/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import SidebarSlider from '../components/SidebarSlider.vue';
+</script>
+
+<template>
+  <SidebarSlider msg="hello" />
+  <main></main>
+</template>
