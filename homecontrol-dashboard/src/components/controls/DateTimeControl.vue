a5376b2a3991907b55598645c7f28ebfcb48e08b
PW <mekatrol@wojcik.com.au>
2024-06-10 22:10:47 +1000

    Roman dates

--- homecontrol-dashboard/src/components/controls/DateTimeControl.vue
+++ homecontrol-dashboard/src/components/controls/DateTimeControl.vue
@@ -12,10 +12,20 @@ import { useIntervalTimer } from 'vue-boosted';
 const timeDisplay = ref('');
 const dateDisplay = ref('');
 
+const romanize = (num: number): string => {
+  if (!+num) return '';
+  let digits = String(+num).split(''),
+    key = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM', '', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC', '', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
+    roman = '',
+    i = 3;
+  while (i--) roman = (key[+digits.pop()! + i * 10] || '') + roman;
+  return Array(+digits.join('') + 1).join('M') + roman;
+};
+
 useIntervalTimer(async () => {
   const dt = new Date();
-  timeDisplay.value = dt.toLocaleTimeString('en-AU', { hour12: false });
-  dateDisplay.value = dt.toLocaleDateString('en-AU', { hour12: false });
+  timeDisplay.value = romanize(dt.getHours() + 1) + ' . ' + romanize(dt.getMinutes()) + ' . ' + romanize(dt.getSeconds());
+  dateDisplay.value = romanize(dt.getMonth() + 1) + ' . ' + romanize(dt.getDate()) + ' . ' + romanize(dt.getFullYear()) + 1;
   return true;
 }, 500);
 </script>
