9393142d3b10578427a95cab8397cbba89887192
PW <mekatrol@wojcik.com.au>
2024-12-21 22:25:51 +1100

    Added Home Assistant Addon demo app

---
+++ Mekatrol.HomeAssistantAddon/Mekatrol.HomeAssistantAddon/AddonService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Reflection;
+
+namespace Mekatrol.HomeAssistantAddon;
+
+internal class AddonService(ILogger<AddonService> logger)
+{
+    private readonly ILogger<AddonService> _logger = logger;
+
+    public async Task Execute(CancellationToken stoppingToken)
+    {
+        var currentAssemblyDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+        var executingAssemblyPath = Path.GetFullPath(currentAssemblyDirectory);
+
+        Console.WriteLine($"Executing from '{executingAssemblyPath}'...");
+
+        var tickNo = 1;
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            Console.WriteLine($"Add on tick {tickNo++}...");
+            await Task.Delay(1000, stoppingToken);
+        }
+
+        Console.WriteLine($"Ended execution from '{executingAssemblyPath}'...");
+    }
+}
---
+++ Mekatrol.HomeAssistantAddon/Mekatrol.HomeAssistantAddon/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Mekatrol.HomeAssistantAddon;
+
+/******************************************************************************
+ * A simple Home Assistant Addon for understanding how to set a .NET add on up
+ ******************************************************************************/
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+                services.AddTransient<AddonService>();
+            })
+            .Build();
+
+        Console.WriteLine($"SUPERVISOR_TOKEN: '{Environment.GetEnvironmentVariable("SUPERVISOR_TOKEN")}'");
+
+        var scriptRunner = host.Services.GetRequiredService<AddonService>();
+        var stoppingTokenSource = new CancellationTokenSource();
+        await scriptRunner.Execute(stoppingTokenSource.Token);
+        stoppingTokenSource.Cancel();
+    }
+}
